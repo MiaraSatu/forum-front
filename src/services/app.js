a359fcc02abbd341226a7d6195db078b4439176c
miarasatu <miarasatu@gmail.com>
2022-08-24 12:53:57 +0300

    create app service provider

---
+++ src/services/app.js
@@ -0,0 +1,20 @@
+/**
+ * @description Change symfony error validation to customized error object
+ * @param {Object} symfonyError 
+ * @param {Array} formAttributes 
+ * 
+ * @returns {Object} error
+ */
+const errorSpliter = (symfonyError, formAttributes = []) => {
+    let formError = {}
+    symfonyError.forEach((error) => {
+        formAttributes.forEach((attribute) => {
+            // pour qu'une attribut a une erreur et elle n'a pas encore d'autre erreur
+            if(error.propertyPath == attribute && !(formError[attribute]))
+                formError[attribute] = error.message
+        })
+    })
+    return formError
+}
+
+export {errorSpliter}
