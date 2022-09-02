4a98ed9b151e5aa2ed7531cb41cdc3d229fea2d6
miarasatu <miarasatu@gmail.com>
2022-09-02 22:21:34 +0300

    install fontawesome and use it in main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,23 @@ import router from './router'
 import store from './store'
 import './assets/main.css'
 
-createApp(App).use(store).use(router).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { 
+    faUserSecret,
+    faHouse
+} from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret, faHouse)
+
+createApp(App)
+.use(store)
+.use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
