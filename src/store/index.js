728338b20b18afc25d75aa1d79ccd2a7667daec5
miarasatu <miarasatu@gmail.com>
2022-09-02 22:28:17 +0300

    store user, jwt_token, posts in vuex store with their setters(mutations)

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,34 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: null,
+    jwt_token: null,
+    posts: []
   },
   getters: {
   },
   mutations: {
+    SET_USER(state, user) {
+      if(user)
+        state.user = {...user}
+      else
+        state.user = null
+    },
+    SET_TOKEN(state, token) {
+      state.token = token
+    },
+    SET_POSTS(state, posts) {
+      state.posts = [...posts]
+    }
   },
   actions: {
+    logout(context) {
+      context.commit('SET_TOKEN', null)
+      context.commit('SET_USER', null)
+    },
+    fetchPost(context, posts) {
+      context.commit('SET_POSTS', posts)
+    }
   },
   modules: {
   }
