5321c280e8dcff50ce1c8ee5c11dfeb4a5d2e935
miarasatu <miarasatu@gmail.com>
2022-09-02 22:30:43 +0300

    load vuex data stored [user, jwt_token] by using mapState in HomeView, and load posts by using axios request before mounting component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,34 @@
 <template>
 	<app-nav />
-	Je suis le Home page
+	Je suis le Home page 
+	<div v-if="user">
+		Welcome {{user.fullName}}
+	</div>
 </template>
 
 <script>
 import AppNav from '@/components/Nav.vue'
+import {mapState} from 'vuex'
+import axios from 'axios'
 
 export default {
 	name: 'HomeView',
-	components: {AppNav}
+	components: {AppNav},
+	computed: {
+		...mapState(['user', 'token'])
+	},
+	beforeMount() {
+		axios.get(process.env.VUE_APP_API_URL+'/posts', {
+			headers: {
+				Authorization: this.token
+			}
+		})
+		.then(({data}) => {
+			console.log(data)
+		})
+		.catch((error) => {
+			console.error('error has occured', error)
+		})
+	}
 }
 </script>
