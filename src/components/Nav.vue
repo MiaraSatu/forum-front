db951f523fc53e3fcd80124b3627f819b4ddbac9
miarasatu <miarasatu@gmail.com>
2022-09-02 22:26:16 +0300

    use route named in Nav, get vuex stored data by using mapState, stylize nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,49 @@
 <template>
     <nav>
-		<router-link to="/">Home</router-link>
-		<router-link to="/login">Login</router-link>
-		<router-link to="/register">Register</router-link>
+		<router-link :to="{name: 'home'}">
+			<font-awesome-icon icon="fa-solid fa-house" />
+			Home
+		</router-link>
+		<div class="user-block" v-if="user">
+			<div>{{user.fullName}}</div>
+			<button @click="logout">Logout</button>
+		</div>
+		<div v-else>
+			<router-link :to="{name: 'login'}">sign in</router-link>
+			or
+			<router-link :to="{name: 'register'}">sign up</router-link>
+		</div>
 	</nav>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
-    name: 'AppNav'
+    name: 'AppNav',
+	computed: {
+		...mapState(['user'])
+	},
+	methods: {
+		logout() {
+			this.$store.dispatch('logout')
+		}
+	}
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 nav {
-	// code
+	padding-left: 10%;
+	padding-right: 10%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 	height: 2rem;
+	background-color: rgb(182, 182, 182);
+	.user-block {
+		display: flex;
+		align-items: center;
+	}
 }
 </style>
