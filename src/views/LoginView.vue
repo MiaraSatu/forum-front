c0236c993226ee05ca02fa072bd380def3b91713
miarasatu <miarasatu@gmail.com>
2022-09-02 22:31:35 +0300

    achieve login form html element

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,14 +2,17 @@
     <div class="form-container">
         <div class="interior">
             <login-nav />
-            <form action="">
+            <form @submit.prevent="submitForm">
                 <h1 class="form-title">Login with existing account</h1>
                 <div class="form-description">Don't have account? <router-link to="/register">Sign up</router-link></div>
-                <div class="group">
-                    <input type="email" placeholder="Email">
+                <div class="error-message" v-if="errorMessage">{{errorMessage}}</div>
+                <div class="group" :class="{'has-error': error.email}">
+                    <input type="email" placeholder="Email" v-model="form.email">
+                    <div class="error" v-if="error.email">{{error.email}}</div>
                 </div>
-                <div class="group">
-                    <input type="password" placeholder="Your password">
+                <div class="group" :class="{'has-error': error.clearPassword}">
+                    <input type="password" placeholder="Your password" v-model="form.clearPassword">
+                    <div class="error" v-if="error.clearPassword">{{error.clearPassword}}</div>
                 </div>
                 <button type="submit">Sign In</button>
             </form>
